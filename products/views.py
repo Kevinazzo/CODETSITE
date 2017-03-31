878020e1749f74ab35730777aa478ec2be9a8fd0
Kevinazzo <servidoryei@gmail.com>
2017-03-31 05:13:52 -0600

    CASI
    CASI

--- products/views.py
+++ products/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 from products import models
 
 
-def home(request):
-    return render(request,'Productos.html')
+def home(request, families):
+    families = [
+        ('Tornillo',models.Familia.objects.filter(nombre="Tornillo")),
+        ('Tuerca',models.Familia.objects.filter(nombre="Tuerca")),
+    ]
+    context = [
+        ("Tornillos", models.Subtipo.objects.filter(familia_ID=families['Tornillo'])),
+    ]
+    return render(request,'Productos.html',context)
