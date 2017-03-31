35d0c29c19e5caf808342ebb8d7fab2af3ad13e0
Kevinazzo <servidoryei@gmail.com>
2017-03-31 04:50:04 -0600

    OK2

--- CODET/urls.py
+++ CODET/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     url(r'^$', views.home),
     url(r'^admin/', admin.site.urls),
     url(r'^productos/',include('products.urls')),
+    url(r'^contacto/',views.contacto),
+    url(r'^fabricaciones/',views.fabricaciones),
+    url(r'^planta/',views.planta)
 ]
--- products/views.py
+++ products/views.py
@@ -4,6 +4,3 @@ from products import models
 
 def home(request):
     return render(request,'Productos.html')
-
-def familyDetails(request, familia_id):
-    return HttpResponse("<h1> Details for family "+str(familia_id)+"</h1>")
